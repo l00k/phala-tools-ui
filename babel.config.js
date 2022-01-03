5aed3ba5e65cd7234a728e09fbbf6e35fae0e023
Lukasz Dobrowolski <l00k+github@protonmail.com>
2022-01-03 07:30:03 +0100

    api backend / frontend separation

--- babel.config.js
+++ babel.config.js
@@ -7,9 +7,10 @@ module.exports = {
         [
             '@babel/plugin-proposal-class-properties'
         ],
-        '@babel/plugin-syntax-dynamic-import',
-        '@babel/plugin-syntax-object-rest-spread',
-        '@babel/plugin-proposal-optional-chaining',
+        '@babel/plugin-proposal-dynamic-import',
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-proposal-nullish-coalescing-operator',
+        '@babel/plugin-proposal-optional-chaining'
     ],
     presets: [
         '@vue/cli-plugin-babel/preset',
