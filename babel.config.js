9775d6137161f652d8e9d0f69b110405fe7550e6
Lukasz Dobrowolski <l00k+github@protonmail.com>
2022-01-03 07:34:03 +0100

    api backend / frontend separation

--- babel.config.js
+++ babel.config.js
@@ -7,8 +7,8 @@ module.exports = {
         [
             '@babel/plugin-proposal-class-properties'
         ],
-        '@babel/plugin-proposal-dynamic-import',
-        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-syntax-object-rest-spread',
         '@babel/plugin-proposal-nullish-coalescing-operator',
         '@babel/plugin-proposal-optional-chaining'
     ],
