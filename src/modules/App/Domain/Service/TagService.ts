a4e8a1613537a8e9f64a76f9c8ab71e84a8ab6bc
Lukasz Dobrowolski <l00k+github@protonmail.com>
2022-01-04 08:49:45 +0100

    api backend / frontend separation

--- src/modules/App/Domain/Service/TagService.ts
+++ src/modules/App/Domain/Service/TagService.ts
@@ -1,8 +1,10 @@
 import Tag from '#/App/Domain/Model/Tag';
 import { Inject } from '@inti5/object-manager';
+import * as Api from '@inti5/api-frontend';
 
 
 export default class TagService
+    extends Api.Con
 {
 
     public async find() : Promise<Tag[]>
