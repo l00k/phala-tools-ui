c32c3d9d340b98672c0ccfc9fda0e801ec925a0c
l00k <l00k+github@protonmail.com>
2022-01-30 05:30:59 +0100

    implementation batch

---
+++ src/modules/App/Page/Index.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import BaseComponent from '@inti5/app-frontend/Component/BaseComponent.vue';
+import { Component, Route } from '@inti5/app-frontend/Vue/Annotations';
+
+
+@Route('/', 'index')
+@Component()
+export default class IndexPage
+    extends BaseComponent
+{
+
+    public async mounted ()
+    {
+        this.$router.push({
+            name: 'stakepools_list'
+        });
+    }
+
+}
+</script>
