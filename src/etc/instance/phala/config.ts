a33e57a708f1e812da0cf61f5fa86e6569ca9c44
l00k <l00k+github@protonmail.com>
2022-04-12 04:07:51 +0200

    improve configuration typing

--- src/etc/instance/phala/config.ts
+++ src/etc/instance/phala/config.ts
@@ -4,13 +4,13 @@ import { ModulePolkadotConfig } from '#/Polkadot/Domain/Type/ModuleConfig';
 
 export default {
     api: {
-        baseUrl: `https://phala.100k.dev/api/`
+        baseUrl: 'https://phala.100k.dev/api/'
     },
     module: {
         watchdog: {
             login: {
                 discord: {
-                    authUrl: `https://discord.com/api/oauth2/authorize?client_id=952938945887485992&redirect_uri=https%3A%2F%2Fphala.100k.dev%2Fwatchdog%2Flogin%2Fdiscord&response_type=code&scope=identify`,
+                    authUrl: 'https://discord.com/api/oauth2/authorize?client_id=952938945887485992&redirect_uri=https%3A%2F%2Fphala.100k.dev%2Fwatchdog%2Flogin%2Fdiscord&response_type=code&scope=identify',
                 },
                 telegram: {
                     botName: 'PhalaWatchdogBot',
